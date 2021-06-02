dfa19db7af5c89a05b0ad421e653a85fdb4a2103
parsa84-pb <58140172+parsa84-pb@users.noreply.github.com>
2021-06-02 17:18:01 +0430

    Update settings.py

--- Dango_Ecommerce/settings.py
+++ Dango_Ecommerce/settings.py
@@ -22,11 +22,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%#)ymgx5-_20k9_a(1=^*j3&g7-@9)tzmb(ox82qx5qpbbl^68'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
-# DEBUG = False
-# ALLOWED_HOSTS =  ['localhost', '127.0.0.1']
+# ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS =  ['localhost', '127.0.0.1', '89.38.130.76']
 
 # Application definition
 INSTALLED_APPS = [
